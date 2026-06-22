e1ec48c25787833936f9ebb0b0652d91a94de24e
Delfi-CH <delfi@delfi.dev>
2026-06-22 17:06:23 +0200

    feat: initial drizzle setup

--- belaberung-js-shared/src/messages.ts
+++ belaberung-js-shared/src/messages.ts
@@ -1,38 +1,45 @@
-import { Room } from "./rooms.js"
-import { User } from "./users.js"
+import { Room } from "./rooms.js";
+import { User } from "./users.js";
 
 export class Message {
-    id: number = 0
-    content: string
-    attachment: MessageAttachment
-    timestamp: Date 
-    user: User
-    room: Room
-    constructor(content: string, attachment: MessageAttachment, user: User, room: Room) {
-        this.content = content
-        this.attachment = attachment
-        this.user = user
-        this.room = room
-        this.timestamp = new Date()
-    }
+  id: number = 0;
+  content: string;
+  attachment: MessageAttachment;
+  timestamp: Date;
+  user: User;
+  room: Room;
+  constructor(
+    content: string,
+    attachment: MessageAttachment,
+    user: User,
+    room: Room,
+  ) {
+    this.content = content;
+    this.attachment = attachment;
+    this.user = user;
+    this.room = room;
+    this.timestamp = new Date();
+  }
 }
 
 export enum MessageAttachmentType {
-    None,
-    BinaryFile,
-    TextFile,
-    Document,
-    Hyperlink,
-    Image,
-    Audio,
-    Video
+  None = "None",
+  Mention = "Mention",
+  Reply = "Reply",
+  BinaryFile = "BinaryFile",
+  TextFile = "TextFile",
+  Document = "Document",
+  Hyperlink = "Hyperlink",
+  Image = "Image",
+  Audio = "Audio",
+  Video = "Video",
 }
 
 export class MessageAttachment {
-    type: MessageAttachmentType
-    data: any
-    constructor(type: MessageAttachmentType, data: any) {
-        this.type = type
-        this.data = data
-    }
+  type: MessageAttachmentType;
+  data: any;
+  constructor(type: MessageAttachmentType, data: any) {
+    this.type = type;
+    this.data = data;
+  }
 }
