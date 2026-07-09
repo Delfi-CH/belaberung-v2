788f9b632bc2ab01d6c80ff13ec2b161d2b15173
Delfi-CH <delfi@delfi.dev>
2026-07-09 12:33:02 +0200

    feat: add delete and patch to user router

--- belaberung-go/model/requests.go
+++ belaberung-go/model/requests.go
@@ -16,3 +16,12 @@ type CreateRoomRequest struct {
 	Description string `json:"description"`
 	Domain string `json:"domain"`
 }
+
+type UpdateUserDetailRequest struct {
+	RequestType string `json:"type"`
+	Username *string `json:"username"`
+	Biography *string `json:"biography"`
+	Pronouns *string `json:"pronouns"`
+	OldPassword *string `json:"oldPassword"`
+	NewPassword *string `json:"newPassword"`
+}
--- belaberung-go/route/user_router.go
+++ belaberung-go/route/user_router.go
@@ -129,4 +129,131 @@ func InitUserRouter(router *gin.RouterGroup, db *bun.DB) {
 	})
 
 	//curl -b /tmp/cookies http://localhost:8080/users/1
+
+	router.DELETE("/:id", func(c *gin.Context) {
+		session := sessions.Default(c)
+		sessionUsername := session.Get("username")
+		sessionIsAdministrator := session.Get("admin")
+
+		if sessionUsername == nil {
+			c.String(http.StatusUnauthorized, "not logged in")
+			return
+		}
+
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		user, err := model.GetUserById(context.Background(), db, id)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if user == nil {
+			c.String(http.StatusNotFound, "user not found")
+			return
+		}
+
+		if user.Username != sessionUsername || sessionIsAdministrator == false {
+			c.String(http.StatusForbidden, "invailid permissions")
+			return
+		}
+
+		err = model.DeleteUser(context.Background(), db, id)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		c.String(http.StatusNoContent, "delete successfull")
+	})
+
+	//curl -X DELETE -b /tmp/cookies http://localhost:8080/users/1
+
+	router.PATCH("/:id", func(c *gin.Context) {
+		session := sessions.Default(c)
+		sessionUsername := session.Get("username")
+		sessionIsAdministrator := session.Get("admin")
+
+		if sessionUsername == nil {
+			c.String(http.StatusUnauthorized, "not logged in")
+			return
+		}
+
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+			return
+		}
+		user, err := model.GetUserById(context.Background(), db, id)
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if user == nil {
+			c.String(http.StatusNotFound, "user not found")
+			return
+		}
+
+		if user.Username != sessionUsername || sessionIsAdministrator == false {
+			c.String(http.StatusForbidden, "invailid permissions")
+			return
+		}
+
+		var req model.UpdateUserDetailRequest
+		if err = c.ShouldBindJSON(&req); err != nil {
+			c.String(http.StatusBadRequest, "bad request: "+err.Error())
+			return
+		}
+
+		detailType := req.RequestType
+
+		switch detailType {
+			case "username":
+				if req.Username != nil {
+					user, err = model.UpdateUserUsername(context.Background(), db, id, *req.Username)
+					break
+				} else {
+					c.String(http.StatusBadRequest, "bad request: "+ err.Error())
+					return
+				}
+			case "password":
+				if req.OldPassword != nil || req.NewPassword != nil {
+					user, err = model.UpdateUserPassword(context.Background(), db, id, *req.OldPassword, *req.NewPassword)
+					break
+				} else {
+					c.String(http.StatusBadRequest, "bad request: "+ err.Error())
+					return
+				}
+			case "biography":
+				if req.Biography != nil {
+					user, err = model.UpdateUserBiograpgy(context.Background(), db, id, *req.Biography)
+					break
+				} else {
+					c.String(http.StatusBadRequest, "bad request: "+ err.Error())
+					return
+				}
+			case "pronouns":
+				if req.Pronouns != nil {
+					user, err = model.UpdateUserPronouns(context.Background(), db, id, *req.Pronouns)
+					break
+				} else {
+					c.String(http.StatusBadRequest, "bad request: "+ err.Error())
+					return
+				}
+		}
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		
+		c.JSON(http.StatusOK, &user)
+	})
+
+	//curl -X PATCH -H "Content-Type: application/json" -d '{"type":"pronouns","pronouns":"he/him"}' -b /tmp/cookies http://localhost:8080/users/1
 }
