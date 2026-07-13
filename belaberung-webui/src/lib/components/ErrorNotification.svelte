44f00529f315d6b181a8ce97b646641a7beff9ff
Delfi-CH <delfi@delfi.dev>
2026-07-13 18:52:47 +0200

    refactor: decide on sveltestrap for the styling

--- belaberung-webui/src/lib/components/ErrorNotification.svelte
+++ belaberung-webui/src/lib/components/ErrorNotification.svelte
@@ -3,19 +3,9 @@
 </script>
 
 {#if isVisible}
-	<script lang="ts">
-	import { TriangleAlertIcon } from '@lucide/svelte';
-</script>
 
-<div class="w-full space-y-8">
-	<div class="card preset-outlined-error-500 grid grid-cols-1 items-center gap-4 p-4 lg:grid-cols-[auto_1fr_auto]">
-		<div>
-			<p class="font-bold">Error</p>
-			<p class="text-xs opacity-60">{message}</p>
-		</div>
-		<div class="flex gap-1 justify-end">
-			<button class="btn preset-tonal hover:preset-filled" onclick={()=> onDismiss()}>Dismiss</button>
-		</div>
-	</div>
+<div>
+	<p>{message}</p>
+	<button onclick={()=> onDismiss()}>Dismiss</button>
 </div>
 {/if}
