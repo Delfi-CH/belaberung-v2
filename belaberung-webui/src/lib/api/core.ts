063219065fb48e8061ac2cc8067f8b070a0fed97
Delfi-CH <delfi@delfi.dev>
2026-07-15 19:48:16 +0200

    feat: update room main page in frontend

--- belaberung-webui/src/lib/api/core.ts
+++ belaberung-webui/src/lib/api/core.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getUserID } from './auth';
 
 const backendURL = import.meta.env.DEV ? 'http://localhost:8081' : '/api';
 
@@ -6,3 +7,31 @@ export const api = axios.create({
 	baseURL: backendURL,
 	withCredentials: true
 });
+
+export async function getPublicRooms() {
+	const res = await api.get("/rooms")
+	const uid = getUserID()
+	const res2 = await api.get(`/users/${uid}/joined`)
+	if (res2.data === null) {
+		return res.data
+	}
+	const joinedRoomsId = res2.data.map((e)=> e.Room.id)
+	const notJoinedRooms = res.data.map((e)=>{
+		if (joinedRoomsId.includes(e.id)) {
+			return {} 
+		} else {
+			return e
+		}
+	})
+	return notJoinedRooms
+}
+
+export async function getJoinedRooms() {
+	const uid = getUserID()
+	const res = await api.get(`/users/${uid}/joined`)
+	if (res.data === null) {
+		return []
+	}
+	const joinedRooms = res.data.map((e)=> e.Room)
+	return joinedRooms
+}
--- belaberung-webui/src/routes/rooms/+page.svelte
+++ belaberung-webui/src/routes/rooms/+page.svelte
@@ -1,1 +1,77 @@
-<h1>rooms</h1>
+<script lang="ts">
+	import { getJoinedRooms, getPublicRooms } from '$lib/api/core';
+	import { onMount } from 'svelte';
+	import {
+		Container,
+		Row,
+		Col,
+		Card,
+		CardHeader,
+		CardTitle,
+		CardBody,
+		CardFooter,
+		Button
+	} from '@sveltestrap/sveltestrap';
+
+	let rooms = $state([]);
+	let joinedRooms = $state([]);
+
+	onMount(async () => {
+		const tmpRooms = await getPublicRooms();
+		rooms = tmpRooms;
+		const tmpJoinedRooms = await getJoinedRooms();
+		joinedRooms = tmpJoinedRooms;
+	});
+</script>
+
+<Container>
+	<h1>rooms</h1>
+	<Row>
+		<h2>Joined</h2>
+		<Col>
+			{#if joinedRooms.length <= 1}
+				{#each joinedRooms as room, index (index)}
+					{#if JSON.stringify(room) !== JSON.stringify({})}
+						<Card>
+							<CardHeader>
+								<CardTitle>{room.name}</CardTitle>
+							</CardHeader>
+							<CardBody>
+								{room.description}
+							</CardBody>
+							<CardFooter>
+								<Button>Goto</Button>
+							</CardFooter>
+						</Card>
+					{/if}
+				{/each}
+			{:else}
+				<p>No rooms found...</p>
+			{/if}
+		</Col>
+	</Row>
+	<Row>
+		<h2>Public Rooms</h2>
+		<Col>
+			{#if rooms.length <= 1}
+				{#each rooms as room, index (index)}
+					{#if JSON.stringify(room) !== JSON.stringify({})}
+						<Card>
+							<CardHeader>
+								<CardTitle>{room.name}</CardTitle>
+							</CardHeader>
+							<CardBody>
+								{room.description}
+							</CardBody>
+							<CardFooter>
+								<Button>Join</Button>
+							</CardFooter>
+						</Card>
+					{/if}
+				{/each}
+			{:else}
+				<p>No rooms found...</p>
+			{/if}
+		</Col>
+	</Row>
+</Container>
