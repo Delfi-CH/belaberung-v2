834d490bd9ee28cf9aad0dac85705021891c8ad9
Delfi-CH <delfi@delfi.dev>
2026-07-15 19:45:38 +0200

    feat: add mobile navbar

--- belaberung-webui/src/lib/components/Navbar.svelte
+++ belaberung-webui/src/lib/components/Navbar.svelte
@@ -6,56 +6,90 @@
 		NavbarToggler,
 		NavItem,
 		NavLink,
-        Dropdown,
-        DropdownItem,
-        DropdownMenu,
-        DropdownToggle,
-        Icon
+		Dropdown,
+		DropdownItem,
+		DropdownMenu,
+		DropdownToggle,
+		Collapse,
+		Icon
 	} from '@sveltestrap/sveltestrap';
-    import { page } from '$app/state';
-    import { getUsername, isLoggedIn } from '$lib/api/auth';
+	import { page } from '$app/state';
+	import { getUsername, isLoggedIn } from '$lib/api/auth';
 
-    let isUserLoggedIn = $state(false);
-    let username = $state("")
+	let isUserLoggedIn = $state(false);
+	let isMobile = $state(false);
+	let isOpen = $state(false);
+	let username = $state('');
 
-    async function updateLogin() {
-        isUserLoggedIn = await isLoggedIn();
-    }
+	async function updateLogin() {
+		isUserLoggedIn = await isLoggedIn();
+	}
 
-    $effect(() => {
-        page.route.id;
+	$effect(() => {
+		page.route.id;
+		isMobile = window.innerWidth < 768;
 
-        void updateLogin();
+		void updateLogin();
 
-        username = getUsername()
-    });
+		username = getUsername();
+	});
 </script>
 
 <Navbar>
 	<NavbarBrand>belaberung</NavbarBrand>
-	<Nav pills>	
-		{#if isUserLoggedIn}
-            <NavItem>
-			    <NavLink href="/" active={page.route.id == "/"}>Home</NavLink>
-		    </NavItem>
-			<NavItem>
-				<NavLink href="/rooms" active={page.route.id == "/rooms"}>Rooms</NavLink>
-			</NavItem>
-            <Dropdown autoClose="manual" isOpen={false}>
-                <DropdownToggle caret><Icon name="person-circle"/> {username}
-                </DropdownToggle>
-                <DropdownMenu>
-                    <DropdownItem>Your Profile</DropdownItem>
-                    <DropdownItem href="/logout" class="bg-danger text-white">Logout</DropdownItem>
-                </DropdownMenu>
-            </Dropdown>
-		{:else}
-			<NavItem>
-				<NavLink href="/login"  active={page.route.id == "/login"}>Login</NavLink>
-			</NavItem>
-			<NavItem>
-				<NavLink href="/register"  active={page.route.id == "/register"}>Register</NavLink>
-			</NavItem>
-		{/if}
-	</Nav>
+
+	{#if isMobile}
+		<NavbarToggler onclick={() => (isOpen = !isOpen)} />
+		<Collapse {isOpen} navbar>
+			<Nav class="text-end" navbar>
+				{#if isUserLoggedIn}
+					<NavItem>
+						<NavLink href="/" active={page.route.id == '/'}>Home</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="/rooms" active={page.route.id == '/rooms'}>Rooms</NavLink>
+					</NavItem>
+					<Dropdown autoClose="manual" isOpen={false}>
+						<DropdownToggle caret><Icon name="person-circle" /> {username}</DropdownToggle>
+						<DropdownMenu>
+							<DropdownItem>Your Profile</DropdownItem>
+							<DropdownItem href="/logout" class="bg-danger text-white">Logout</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				{:else}
+					<NavItem>
+						<NavLink href="/login" active={page.route.id == '/login'}>Login</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink href="/register" active={page.route.id == '/register'}>Register</NavLink>
+					</NavItem>
+				{/if}
+			</Nav>
+		</Collapse>
+	{:else}
+		<Nav pills>
+			{#if isUserLoggedIn}
+				<NavItem>
+					<NavLink href="/" active={page.route.id == '/'}>Home</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="/rooms" active={page.route.id == '/rooms'}>Rooms</NavLink>
+				</NavItem>
+				<Dropdown autoClose="manual" isOpen={false}>
+					<DropdownToggle caret><Icon name="person-circle" /> {username}</DropdownToggle>
+					<DropdownMenu>
+						<DropdownItem>Your Profile</DropdownItem>
+						<DropdownItem href="/logout" class="bg-danger text-white">Logout</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			{:else}
+				<NavItem>
+					<NavLink href="/login" active={page.route.id == '/login'}>Login</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="/register" active={page.route.id == '/register'}>Register</NavLink>
+				</NavItem>
+			{/if}
+		</Nav>
+	{/if}
 </Navbar>
